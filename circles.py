b4c47d012456eda4eb21c8ea9f1f3183aaf8b908
bright-tools <dev@brightsilence.com>
2017-01-17 15:40:41 +0000

    Add option to use rainbow colouring for internal circles

--- circles.py
+++ circles.py
@@ -12,7 +12,7 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
-import pygame, sys, math, time
+import pygame, sys, math, time, colorsys
 from pygame.locals import *
 
 # Colours, defined in RGB triplets
@@ -30,6 +30,8 @@ DEGREES_IN_ROTATION = 360
 EFFECT = 2
 MAX_EFFECT = 2
 TIME_STEP = 0.02
+
+enableColour = False
 
 class Circle:
     def __init__(self,center,diameter):
@@ -58,12 +60,18 @@ class Anim:
             self.drawLine( lineCount * self.spacing )
 
     def drawInnerCircle(self,angle,offset):
+        global enableColour
+
         rads = math.radians(angle)
         diam = (self.outerCircle.diameter / DEGREES_IN_ROTATION) * (offset)
         startx = (int)(self.outerCircle.center[0] + math.sin(rads)*diam)
         starty = (int)(self.outerCircle.center[1] + math.cos(rads)*diam)
         circle = Circle( [startx, starty], INNER_CIRCLE_DIAMETER )
-        circle.draw( self.windowSurface, INNER_CIRCLE_COLOUR, INNER_CIRCLE_THICKNESS )
+        if enableColour:
+            col = tuple([255*x for x in colorsys.hsv_to_rgb(angle/180, 1, 1)])
+        else:
+            col = INNER_CIRCLE_COLOUR
+        circle.draw( self.windowSurface, col, INNER_CIRCLE_THICKNESS )
 
     def drawCircleEffect1(self,angle):
         offset = (angle + self.step ) % DEGREES_IN_ROTATION
@@ -107,7 +115,7 @@ class Anim:
         pygame.display.update()
 
 def main():
-    global LINES, EFFECT
+    global LINES, EFFECT, enableColour
     pygame.init()
     windowSurface = pygame.display.set_mode((500, 500), 0, 32)
     pygame.display.set_caption('Keep your eyes on the circles ...')
@@ -131,6 +139,8 @@ def main():
                 if event.key == pygame.K_UP:
                     if( EFFECT < MAX_EFFECT ):
                         EFFECT = EFFECT + 1
+                if event.key == pygame.K_c:
+                    enableColour = not enableColour
             elif event.type == QUIT:
                 pygame.quit()
                 sys.exit()
